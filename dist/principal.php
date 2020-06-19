a43c3f28b4e62315748c0c5c47421533e41300f4
Rodrigo <rodrigo.coninck@hotmail.com>
2020-06-18 22:09:19 -0300

    Pagina Principal

--- dist/principal.php
+++ dist/principal.php
@@ -57,12 +57,11 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 bg-danger">
+            <div class="col-md-6">
                 <div class="row">
-                    <div class="col-md-12 bg-dark">
+                    <div class="col-md-12 bg-secondary rounded">
                         <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Example textarea</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <textarea class="form-control my-3" id="exampleFormControlTextarea1" rows="1" placeholder="Compartilhe algo"></textarea>
                         </div>
                         <div class="row">
                             <div class="col-md-4">
@@ -74,6 +73,7 @@
                             <div class="col-md-5">
                                 <div class="form-group">
                                     <select class="form-control" id="exampleFormControlSelect1">
+                                        <option selected>Escolha o jogo...</option>
                                         <option>League Of Legends</option>
                                         <option>CS:GO</option>
                                         <option>Tibia</option>
@@ -89,25 +89,64 @@
                     </div>
                 </div>
                 <div class="col-md-12">
-                    <div class="card my-3">
+                    <div class="card p-3 border my-3">
                         <div class="card-body">
-                            <h5 class="card-title">Título do card</h5>
-                            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
-                            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
-                        </div>
-                        <img class="card-img-bottom" src="https://i.pinimg.com/originals/2b/8a/45/2b8a454e233a534d724c78e3f4914abd.jpg" alt="Imagem de capa do card">
-                    </div>
-                    <div class="card p-3">
-                        <div class="card-body">
-                            <h5 class="card-title">Título do card</h5>
                             <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
                             <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
                         </div>
                         <img class="card-img-bottom" src="https://i.pinimg.com/originals/2b/8a/45/2b8a454e233a534d724c78e3f4914abd.jpg" alt="Imagem de capa do card">
                         <div class="form-group my-1">
-                            <label for="exampleFormControlTextarea">Comentar</label>
-                            <button class="btn btn-success btn-sm">Like</button>
-                            <textarea class="form-control" id="exampleFormControlTextarea" rows="1"></textarea>
+                            <button class="btn btn-primary btn-sm">Like</button>
+                            <br>
+                            <textarea class="form-control my-1" id="exampleFormControlTextarea" rows="1" placeholder="Deixe seu comentário"></textarea>
+                            <label for="">Comentários</label>
+                            <button class="btn btn-success btn-sm float-right" for="exampleFormControlTextarea">Enviar</button>
+                        </div>
+                        <div class="border">
+                            <img src="https://lh3.googleusercontent.com/proxy/8E58nNL9vv4o2cp7F6GX6jPU3eg4mXcXDC86kie5HPhDhMOv7xdjymjpfyP8x7xHe6UnDlKYKYGQTM5kMMjrgXs5xWV3tGCVzwyApLqGUizxpdRPR-u1VJRwXz4ncNB0KWibXtq07swUAenL3a7orFxudnXa6nF6cODnVmEXNHtJ9EM4Eyuv" alt="" width="40" height="40">
+                            <label for="">Nome do comentarista</label>
+                            <hr>
+                            <div class="col-md-12">
+                                <label for="">Esse avião é muito supimpa Esse avião é muito supimpa Esse avião é muito supimpa</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card p-3 border my-3">
+                        <div class="card-body">
+                            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
+                            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+                        </div>
+                        <img class="card-img-bottom" src="https://i.pinimg.com/originals/2b/8a/45/2b8a454e233a534d724c78e3f4914abd.jpg" alt="Imagem de capa do card">
+                        <div class="form-group my-1">
+                            <button class="btn btn-primary btn-sm">Like</button>
+                            <br>
+                            <textarea class="form-control my-1" id="exampleFormControlTextarea" rows="1" placeholder="Deixe seu comentário"></textarea>
+                            <label for="">Comentários</label>
+                            <button class="btn btn-success btn-sm float-right" for="exampleFormControlTextarea">Enviar</button>
+                        </div>
+                        <div class="border my-3">
+                            <img src="https://lh3.googleusercontent.com/proxy/8E58nNL9vv4o2cp7F6GX6jPU3eg4mXcXDC86kie5HPhDhMOv7xdjymjpfyP8x7xHe6UnDlKYKYGQTM5kMMjrgXs5xWV3tGCVzwyApLqGUizxpdRPR-u1VJRwXz4ncNB0KWibXtq07swUAenL3a7orFxudnXa6nF6cODnVmEXNHtJ9EM4Eyuv" alt="" width="40" height="40">
+                            <label for="">Nome do comentarista</label>
+                            <hr>
+                            <div class="col-md-12">
+                                <label for="">Esse avião é muito supimpa Esse avião é muito supimpa Esse avião é muito supimpa</label>
+                            </div>
+                        </div>
+                        <div class="border my-3">
+                            <img src="https://lh3.googleusercontent.com/proxy/8E58nNL9vv4o2cp7F6GX6jPU3eg4mXcXDC86kie5HPhDhMOv7xdjymjpfyP8x7xHe6UnDlKYKYGQTM5kMMjrgXs5xWV3tGCVzwyApLqGUizxpdRPR-u1VJRwXz4ncNB0KWibXtq07swUAenL3a7orFxudnXa6nF6cODnVmEXNHtJ9EM4Eyuv" alt="" width="40" height="40">
+                            <label for="">Arnaldo Cesar Coelho</label>
+                            <hr>
+                            <div class="col-md-12">
+                                <label for="">Esse avião é muito supimpa Esse avião é muito supimpa Esse avião é muito supimpa</label>
+                            </div>
+                        </div>
+                        <div class="border my-3">
+                            <img src="https://lh3.googleusercontent.com/proxy/8E58nNL9vv4o2cp7F6GX6jPU3eg4mXcXDC86kie5HPhDhMOv7xdjymjpfyP8x7xHe6UnDlKYKYGQTM5kMMjrgXs5xWV3tGCVzwyApLqGUizxpdRPR-u1VJRwXz4ncNB0KWibXtq07swUAenL3a7orFxudnXa6nF6cODnVmEXNHtJ9EM4Eyuv" alt="" width="40" height="40">
+                            <label for="">Galvão</label>
+                            <hr>
+                            <div class="col-md-12">
+                                <label for="">Pode isso Arnaldo?</label>
+                            </div>
                         </div>
                     </div>
                 </div>
