92abda29ff76bb3620f55b300d1438709bd4c1de
Rodrigo <rodrigo.coninck@hotmail.com>
2020-06-18 21:22:07 -0300

    Página Principal

--- dist/principal.php
+++ dist/principal.php
@@ -1,18 +1,131 @@
 <!doctype html>
 <html lang="pr-br">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- CSS -->
-        <link rel="stylesheet" href="../src/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../src/css/main.css">
-        <title>TNGamers</title>
-    </head>
-    <body>
-        <h1>teste</h1>
-        <!-- Optional JavaScript -->
-        <script src="../src/js/jquery-3.5.1.slim.min.js"></script>
-        <script src="../src/js/popper.min.js"></script>
-        <script src="../src/js/bootstrap.min.js"></script>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../src/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../src/css/main.css">
+    <title>TNG - Login</title>
+</head>
+
+<body class="">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Container</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExample07">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown07">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-md-0">
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="col-md-12 bg-dark pt-3 rounded">
+                    <div class="card">
+                        <img class="card-img-top" src="https://img.freepik.com/vetores-gratis/imagem-realista-de-um-raio-eletrico-starburst_1284-21466.jpg?size=338&ext=jpg" alt="Imagem de capa do card">
+                        <div class="card-body bg-danger">
+                            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 bg-danger">
+                <div class="row">
+                    <div class="col-md-12 bg-dark">
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Example textarea</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input">
+                                    <label class="custom-file-label" for="customFile">Imagem</label>
+                                </div>
+                            </div>
+                            <div class="col-md-5">
+                                <div class="form-group">
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>League Of Legends</option>
+                                        <option>CS:GO</option>
+                                        <option>Tibia</option>
+                                        <option>Ragnarok</option>
+                                        <option>Hotel Habbo</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <button class="btn btn-success btn-block">Publicar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="card my-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Título do card</h5>
+                            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
+                            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+                        </div>
+                        <img class="card-img-bottom" src="https://i.pinimg.com/originals/2b/8a/45/2b8a454e233a534d724c78e3f4914abd.jpg" alt="Imagem de capa do card">
+                    </div>
+                    <div class="card p-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Título do card</h5>
+                            <p class="card-text">Este é um card maior com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional. Este conteúdo é um pouco maior, para demonstração.</p>
+                            <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+                        </div>
+                        <img class="card-img-bottom" src="https://i.pinimg.com/originals/2b/8a/45/2b8a454e233a534d724c78e3f4914abd.jpg" alt="Imagem de capa do card">
+                        <div class="form-group my-1">
+                            <label for="exampleFormControlTextarea">Comentar</label>
+                            <button class="btn btn-success btn-sm">Like</button>
+                            <textarea class="form-control" id="exampleFormControlTextarea" rows="1"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 bg-dark">
+                <h1>Teste 3</h1>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+
+<!-- Optional JavaScript -->
+<script src="../src/js/jquery-3.5.1.slim.min.js"></script>
+<script src="../src/js/popper.min.js"></script>
+<script src="../src/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
